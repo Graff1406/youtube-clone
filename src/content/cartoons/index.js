ba92e9511380889637864d3a0ab4589ffa67589f
avtandil <shainidze@gmail.com>
2022-10-29 15:18:18 +0300

    add deveop content

--- src/content/cartoons/index.js
+++ src/content/cartoons/index.js
@@ -1,5 +1,6 @@
 import PAW from "./PAW";
 import oddbods from "./oddbods";
 import different from "./different";
+import develop from "../develop";
 
-export default different.concat(PAW, oddbods, PAW, oddbods, different);
+export default different.concat(PAW, oddbods, PAW, oddbods, different, develop);
